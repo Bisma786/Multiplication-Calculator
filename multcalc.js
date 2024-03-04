ae33fa4a24becf8bf4761ac2f78f6c41a9dbefe4
Bisma Gondal <159830145+Bisma786@users.noreply.github.com>
2024-03-05 00:25:07 +0500

    Add files via upload

---
+++ multcalc.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+const input1 = await inquirer.prompt({
+    name: "numb1",
+    type: "number",
+    message: "Enter your fist number",
+});
+const input2 = await inquirer.prompt({
+    name: "numb2",
+    type: "number",
+    message: "Enter your 2nd value"
+});
+let total = input1.numb1 * input2.numb2;
+console.log(total);
